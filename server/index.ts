5066d67f0f9d2c07229599b2f71cb054f153bd0b
Arboni <arboniguitar@gmai.com>
2025-06-12 16:08:33 -0300

    up to date .dist

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
 import { seedDefaultAdmin } from "./seed";
 
 // Set the WhatsApp provider to CallMeBot as default
@@ -34,7 +33,7 @@ app.use((req, res, next) => {
         logLine = logLine.slice(0, 79) + "…";
       }
 
-      log(logLine);
+      console.log(logLine);
     }
   });
 
@@ -55,14 +54,7 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
